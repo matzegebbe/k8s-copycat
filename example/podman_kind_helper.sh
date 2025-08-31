c9622f37f823a4b2f1e114145ade38e8f8fcebd5
Mathias Gebbe <mathias.gebbe@gmail.com>
2025-08-31 22:55:42 +0200

    chore: modify kind helper

--- example/podman_kind_helper.sh
+++ example/podman_kind_helper.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-podman build -t ghcr.io/matzegebbe/k8s-copycat:1 .
-podman save ghcr.io/matzegebbe/k8s-copycat:1 -o k8s-copycat.tar
+# Determine the tag
+if [ -n "$1" ]; then
+  TAG=$1
+elif git rev-parse --abbrev-ref HEAD >/dev/null 2>&1; then
+  TAG=$(git rev-parse --abbrev-ref HEAD)
+else
+  TAG="latest"
+fi
+
+podman build -t ghcr.io/matzegebbe/k8s-copycat:$TAG .
+podman save ghcr.io/matzegebbe/k8s-copycat:$TAG -o k8s-copycat.tar
 kind load image-archive k8s-copycat.tar --name kind
